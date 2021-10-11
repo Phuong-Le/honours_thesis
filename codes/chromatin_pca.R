a197d01e9145a648c52a5d4325872d1af6aade28
Phuong Le <alphn2015@gmail.com>
2021-10-11 14:52:51 +1100

    MAINT: changed cell type name

--- codes/chromatin_pca.R
+++ codes/chromatin_pca.R
@@ -14,12 +14,30 @@ stopifnot(test_matrix == t(test_matrix))
 #pca
 dimensions = 8
 pca = distance_table_to_pca(distance_matrix,dimensions)
+# PC 2 vs PC1
 pc.x = 1
 pc.y = 2
 pca = rownames_to_column(pca, var = 'cancer_type')
-ggplot(data = pca, mapping = aes(x=pca[,paste('PC',pc.x,sep='')],y=pca[,paste('PC',pc.y,sep='')], label=cancer_type)) +
+p1 = ggplot(data = pca, mapping = aes(x=pca[,paste('PC',pc.x,sep='')],y=pca[,paste('PC',pc.y,sep='')], label=cancer_type)) +
   geom_text_repel() +
   geom_point(colour = '#B5739D') +
   labs(x=paste('principal coordinate ',pc.x, ' (',round((variance_proportion(pca,dimensions)[pc.x]),2),'% explained)',sep=''),
-       y=paste('principal coordinate ', pc.y, ' (',round((variance_proportion(pca,2)[dimensions]),2),'% explained)',sep=''))
+       y=paste('principal coordinate ', pc.y, ' (',round((variance_proportion(pca,dimensions)[pc.y]),2),'% explained)',sep=''))
 
+pdf('/Users/phuongle/Documents/repos/honours_thesis/graphics/encode_pca_1_2.pdf',width=4.5,height=5)
+p1
+dev.off()
+
+# PC3 vs PC1
+pc.x = 1
+pc.y = 3
+p2 = ggplot(data = pca, mapping = aes(x=pca[,paste('PC',pc.x,sep='')],y=pca[,paste('PC',pc.y,sep='')], label=cancer_type)) +
+  geom_text_repel() +
+  geom_point(colour = '#B5739D') +
+  labs(x=paste('principal coordinate ',pc.x, ' (',round((variance_proportion(pca,dimensions)[pc.x]),2),'% explained)',sep=''),
+       y=paste('principal coordinate ', pc.y, ' (',round((variance_proportion(pca,dimensions)[pc.y]),2),'% explained)',sep=''))
+p2
+
+pdf('/Users/phuongle/Documents/repos/honours_thesis/graphics/encode_pca_1_3.pdf',width=4.5,height=5)
+p2
+dev.off()
